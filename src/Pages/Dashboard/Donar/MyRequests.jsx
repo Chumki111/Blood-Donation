62286ce0eb7b29c2448ce9974f567e3640a0b4c1
Chumki khatun <chumkikhatun370@gmail.com>
2024-03-17 07:34:21 +0600

    complete details page of donation

--- src/Pages/Dashboard/Donar/MyRequests.jsx
+++ src/Pages/Dashboard/Donar/MyRequests.jsx
@@ -2,47 +2,63 @@ import { useEffect, useState } from "react";
 import useAuth from "../../../Hooks/useAuth"
 import { getDonarDonations } from "../../../api/donations";
 import DonationDataRow from "../../../Components/Table/DonationDataRow";
-
+import { FaArrowsSpin } from "react-icons/fa6";
 
 const MyRequests = () => {
-  const {user} = useAuth();
-  const [donations,setDonations] = useState([]);
-  useEffect(() =>{
-   getDonarDonations(user?.email)
-   .then(data=>{
-    
-    setDonations(data);
-   })
-  },[user])
+  const { user } = useAuth();
+  const [loading,setLoading] = useState(false)
+  const [donations, setDonations] = useState([]);
+  useEffect(() => {
+    setLoading(true)
+    getDonarDonations(user?.email)
+      .then(data => {
+
+        setDonations(data);
+        setLoading(false)
+      })
+  }, [user])
+ 
   return (
     <>
-    
-      <div className="overflow-x-auto">
-  <table className="min-w-full border border-gray-200 bg-white shadow-lg">
-    {/* Table Header */}
-      <thead>
-          <tr className="h-[70px] text-sm capitalize border-b bg-rose-500 text-[#FFFFFF]">
-              
+    {loading ? (<div className="flex justify-center items-center h-screen">
+          <FaArrowsSpin className="text-3xl animate-spin text-rose-600"/>
+        </div>) : ( <div className="overflow-x-auto">
+        <table className="min-w-full border border-gray-200 bg-white shadow-lg">
+          {/* Table Header */}
+          <thead>
+            <tr className="h-[70px] text-sm capitalize border-b bg-rose-500 text-[#FFFFFF]">
+
               <th className="px-4 py-4 text-start">recipient</th>
               <th className="px-4 py-4 text-start">Status</th>
               <th className="px-4 py-4 text-start">Hospital</th>
               <th className="px-4 py-4 text-start">Donation Date</th>
               <th className="px-4 py-4 text-start">Address</th>
               <th className="px-4 py-4 text-start">Donation Time</th>
-        
+
               <th className="px-4 py-4 text-start">Update</th>
               <th className="px-4 py-4 text-start">Details</th>
               <th className="px-4 py-4 text-start">Delete</th>
-          </tr>
-      </thead>
-      <tbody className="text-sm">
-        {/* Table rows */}
-        {
-                    donations?.map(donation =><DonationDataRow key={donation._id}  donation={donation}/>)
-                  }
-      </tbody>
-  </table>
-</div>
+            </tr>
+          </thead>
+          <tbody className="text-sm">
+            {/* Table rows */}
+            {/* Conditional rendering based on donations */}
+            {donations.length > 0 ? (
+              donations.map(donation => (
+                <DonationDataRow key={donation._id} donation={donation} />
+              ))
+            ) : (
+              <tr>
+                <td colSpan="9" className="px-4 py-4 text-center text-2xl text-rose-500">
+                You have not made any donation requests.
+                </td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </div>)}
+
+     
     </>
   )
 }
--- src/Pages/Dashboard/Donar/ViewRequest.jsx
+++ src/Pages/Dashboard/Donar/ViewRequest.jsx
@@ -5,9 +5,84 @@ import { useLoaderData} from "react-router-dom"
 const ViewRequest = () => {
     const donation=useLoaderData();
     console.log(donation);
+    const { donation_date } = donation;
+    const donationDateTime = donation_date.toString(); // Convert to string
+    const datePart = donationDateTime.split(',')[0].trim();
+    const timePart = donationDateTime.split(',')[1].trim();
   return (
     <div>
-     
+     <h2 className="font-semibold text-2xl sm:text-xl md:text-4xl lg:text-5xl text-red-500">Donation Information</h2>
+     <div className="flex mt-12 ml-6 flex-col gap-4">
+                <div className="flex items-start">
+                    <p className="text-xl mr-4">
+                        <span className="font-semibold">Requester Name :</span>
+                    </p>
+                    <p className="text-xl">{donation?.requester_Name}</p>
+                </div>
+                <div className="flex items-start">
+                    <p className="text-xl mr-4">
+                        <span className="font-semibold">Requester Email :</span>
+                    </p>
+                    <p className="text-xl">{donation?.requester_email}</p>
+                </div>
+                
+                <div className="flex items-start">
+                    <p className="text-xl mr-4">
+                        <span className="font-semibold">District :</span>
+                    </p>
+                    <p className="text-xl">{donation?.recipient_district}</p>
+                </div>
+                
+                <div className="flex items-start">
+                    <p className="text-xl mr-4">
+                        <span className="font-semibold">Upazila :</span>
+                    </p>
+                    <p className="text-xl">{donation?.recipient_upazila}</p>
+                </div>
+                
+                <div className="flex items-start">
+                    <p className="text-xl mr-4">
+                        <span className="font-semibold">Donation Time :</span>
+                    </p>
+                    <p className="text-xl">{timePart}</p>
+                </div>
+                
+                <div className="flex items-start">
+                    <p className="text-xl mr-4">
+                        <span className="font-semibold">Donation Date :</span>
+                    </p>
+                    <p className="text-xl">{datePart}</p>
+                </div>
+                
+                <div className="flex items-start">
+                    <p className="text-xl mr-4">
+                        <span className="font-semibold">Hospital Name :</span>
+                    </p>
+                    <p className="text-xl">{donation?.hospital_name}</p>
+                </div>
+                
+                <div className="flex items-start">
+                    <p className="text-xl mr-4">
+                        <span className="font-semibold">Full Address :</span>
+                    </p>
+                    <p className="text-xl">{donation?.full_address}</p>
+                </div>
+                
+                <div className="flex items-start">
+                    <p className="text-xl mr-4">
+                        <span className="font-semibold">Donation Status :</span>
+                    </p>
+                    <p className="text-xl">{donation?.donation_status}</p>
+                </div>
+                
+                <div className="flex items-start">
+                    <p className="text-xl mr-4">
+                        <span className="font-semibold">Message:</span>
+                    </p>
+                    <p className="text-xl text-justify">{donation?.request_message}</p>
+                </div>
+            </div>
+             
     </div>
   )
 }
