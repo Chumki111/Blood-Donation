7a049091ea4d7523b64cfd882e119f507d0de4b2
Chumki khatun <chumkikhatun370@gmail.com>
2024-03-25 22:23:39 +0600

    create admin home

--- src/Components/Dashboard/Sidebar/AdminHome.jsx
+++ src/Components/Dashboard/Sidebar/AdminHome.jsx
@@ -6,33 +6,33 @@ import { getTotalAmount } from "../../../api/payment";
 import { useEffect, useState } from "react";
 
 const AdminHome = () => {
-  const [amounts,setAmounts] = useState([])
+  const [amounts, setAmounts] = useState([])
 
-  const {data:users} = useQuery({
-    queryKey:['users'],
-    queryFn:async() => getAllUsers()
-
-  })
-  const {data:donations} = useQuery({
-    queryKey:['donations'],
-    queryFn:async() => getAllDonation()
+  const { data: users } = useQuery({
+    queryKey: ['users'],
+    queryFn: async () => getAllUsers()
 
   })
-   useEffect(() =>{
-   getTotalAmount()
-   .then(data =>{
-  const totalAmount =data?.reduce((total,fund) => total + parseInt(fund.fund),0)
-  setAmounts(totalAmount)
-   })
-   },[])
+  const { data: donations } = useQuery({
+    queryKey: ['donations'],
+    queryFn: async () => getAllDonation()
+
+  })
+  useEffect(() => {
+    getTotalAmount()
+      .then(data => {
+        const totalAmount = data?.reduce((total, fund) => total + parseInt(fund.fund), 0)
+        setAmounts(totalAmount)
+      })
+  }, [])
 
   return (
     <>
-     <h2>{users?.length}</h2>
-     <h2>{donations?.length}</h2>
-     <h2>$ {amounts}</h2>
-     
-    
+      <h2>{users?.length}</h2>
+      <h2>{donations?.length}</h2>
+      <h2>$ {amounts}</h2>
+
+
     </>
   )
 }
