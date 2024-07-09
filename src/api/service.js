cfa85c95e21f03ccc04fa7ed868cab5f536d74a0
Chumki khatun <chumkikhatun370@gmail.com>
2024-07-09 23:34:17 +0600

    fetch single service

--- src/api/service.js
+++ src/api/service.js
@@ -5,3 +5,8 @@ export const getAllServices=async()=>{
     const {data} = await axiosSecure('/services');
     return data;
 }
+// fetch all donations
+export const getSingleService=async(id)=>{
+    const {data} = await axiosSecure(`/service/${id}`);
+    return data;
+}
