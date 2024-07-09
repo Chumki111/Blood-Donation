8980e7588c9c8a718ebfbf09fbb797689e8d49b6
Chumki khatun <chumkikhatun370@gmail.com>
2024-07-09 15:16:26 +0600

    add primary hero payment page

--- src/Pages/Payment/Payment.jsx
+++ src/Pages/Payment/Payment.jsx
@@ -3,16 +3,18 @@ import { Elements } from '@stripe/react-stripe-js'
 import { loadStripe } from '@stripe/stripe-js';
 import useAuth from "../../Hooks/useAuth"
 import CheckoutForm from "./CheckForm";
-const stripePromise=loadStripe(import.meta.env.VITE_PAYMENT_GETWAY_PK)
+import PrimaryHero from '../../Components/Hero/PrimaryHero';
+const stripePromise = loadStripe(import.meta.env.VITE_PAYMENT_GETWAY_PK)
 const Payment = () => {
-    const {user} =useAuth();
+  const { user } = useAuth();
   return (
     <div>
-        <h2 className="text-center font-semibold text-4xl my-5">Welcome Back <span className="text-red-600">{user?.displayName}</span></h2>
-        <h2 className="text-2xl font-medium text-center"><span className="text-red-500">Payment</span> Donation</h2>
-        <Elements stripe={stripePromise}>
-    <CheckoutForm/>
-        </Elements>
+      <PrimaryHero text="Payment Donation" />
+      <h2 className="text-center font-semibold text-4xl my-5">Welcome Back <span className="text-red-600">{user?.displayName}</span></h2>
+      <h2 className="text-2xl font-medium text-center"><span className="text-red-500">Payment</span> Donation</h2>
+      <Elements stripe={stripePromise}>
+        <CheckoutForm />
+      </Elements>
     </div>
   )
 }
