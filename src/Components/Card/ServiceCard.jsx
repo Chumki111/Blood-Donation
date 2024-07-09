287826ef40abcbdfa9e2997520160af0e7bbd7b4
Chumki khatun <chumkikhatun370@gmail.com>
2024-07-09 23:28:11 +0600

    complete service section with responsive

--- src/Components/Card/ServiceCard.jsx
+++ src/Components/Card/ServiceCard.jsx
@@ -1,10 +1,10 @@
 const ServiceCard = ({ service, reverse ,index}) => {
     return (
-      <div className={`flex ${reverse ? 'flex-col md:flex-row-reverse text-right' : 'flex-col md:flex-row'} items-start gap-10`}>
+      <div className={`flex ${reverse ? 'flex-col lg:flex-row-reverse text-right' : 'flex-col lg:flex-row'} items-start lg:gap-10`}>
         
-        <img src={service.image} alt={service.title} className="w-full h-[90%] object-cover mb-4 rounded-lg" />
-        <div className="flex flex-col ml-4 py-5">
-            <h1 className="text-[#e7e7e7] text-7xl font-bold py-4">0{index+1}</h1>
+        <img src={service.image} alt={service.title} className="w-full  h-[60%] lg:h-[90%] object-cover lg:mb-4 rounded-lg" />
+        <div className="flex flex-col ml-4 lg:py-5">
+            <h1 className="text-[#e7e7e7] text-6xl md:text-7xl font-bold py-4">0{index+1}</h1>
           <h3 className="text-4xl font-bold mb-4">{service.title}</h3>
           <p className="text-gray-600 text-justify">{service.description}</p>
           <div className='pt-7'>
--- src/Components/Heading/Heading.jsx
+++ src/Components/Heading/Heading.jsx
@@ -1,10 +1,8 @@
-
-
 const Heading = ({heading,subHeading}) => {
   return (
     <div className="text-center">
          <h2 className="font-medium text-primary">{subHeading}</h2>
-         <h2 className="text-5xl font-bold py-6">{heading}</h2>
+         <h2 className="text-3xl md:text-5xl font-bold py-6">{heading}</h2>
     </div>
   )
 }
