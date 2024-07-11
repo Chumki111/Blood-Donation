75d473da3d06028ef042b121f417e6c5975bb105
Chumki khatun <chumkikhatun370@gmail.com>
2024-07-11 13:59:14 +0600

    get all blogs by query

--- src/Components/Home/BlogsSection.jsx
+++ src/Components/Home/BlogsSection.jsx
@@ -1,9 +1,16 @@
+import { useQuery } from "@tanstack/react-query"
 import Heading from "../Heading/Heading"
+import { getAllBlogs } from "../../api/blogs"
 
 
 const BlogsSection = () => {
+    const {data : blogs} = useQuery({
+        queryKey :['blogs'],
+        queryFn: async () => getAllBlogs()
+    })
+
   return (
-  <section className="bg-gray-100 py-14">
+  <section className="bg-gray-100 py-14 mt-12">
     <div className="container mx-auto px-4 text-center">
     <Heading 
           subHeading="Our Blogs" 
