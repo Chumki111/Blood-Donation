12fa241fd18ee62d1d14570ba4cb1729870d12db
Chumki khatun <chumkikhatun370@gmail.com>
2024-07-11 07:22:19 +0600

    add scrool to top

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,28 +1,35 @@
-import { Outlet } from "react-router-dom"
-import Navbar from "../Components/Shared/Navbar"
-import { useEffect, useState } from "react"
+import { Outlet } from "react-router-dom";
+import Navbar from "../Components/Shared/Navbar";
+import { useEffect, useState } from "react";
 import { FaSpinner } from "react-icons/fa";
 import Footer from "../Components/Shared/Footer";
+import ScrollToTop from "react-scroll-to-top";
+
 
 const Main = () => {
-    const [loading,setLoading] = useState(true);
-    useEffect(() =>{
-setTimeout(() =>{
-    setLoading(false)
-},1000)
-    },[]);
+    const [loading, setLoading] = useState(true);
+    useEffect(() => {
+        setTimeout(() => {
+            setLoading(false);
+        }, 1000);
+    }, []);
+
     if (loading) {
-        return <div className='flex justify-center items-center h-screen'>
-          
-          <FaSpinner className='animate-spin m-auto text-4xl text-[#EB2C29]'/></div>; 
-      }
-  return (
-    <div className="max-w-[100rem] mx-auto">
-    <Navbar/>
-    <Outlet/>
-    <Footer/>
-    </div>
-  )
+        return (
+            <div className='flex justify-center items-center h-screen'>
+                <FaSpinner className='animate-spin m-auto text-4xl text-[#EB2C29]' />
+            </div>
+        );
+    }
+
+    return (
+        <div className="max-w-[100rem] mx-auto">
+            <Navbar />
+            <Outlet />
+            <Footer />
+            <ScrollToTop smooth color="#EB2C29" />
+        </div>
+    );
 }
 
-export default Main
+export default Main;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,25 +1,24 @@
-import ContactSection from "../../Components/Home/ContactSection"
-import Countup from "../../Components/Home/Countup"
-import SecondSection from "../../Components/Home/SecondSection"
-import ServicesSection from "../../Components/Home/ServicesSection"
-import TeamMember from "../../Components/Home/TeamMember"
-import Testimonial from "../../Components/Home/Testimonial"
-import Banner from "./Banner"
+import ContactSection from "../../Components/Home/ContactSection";
+import Countup from "../../Components/Home/Countup";
+import SecondSection from "../../Components/Home/SecondSection";
+import ServicesSection from "../../Components/Home/ServicesSection";
+import TeamMember from "../../Components/Home/TeamMember";
+import Testimonial from "../../Components/Home/Testimonial";
+import Banner from "./Banner";
 
 
 const Home = () => {
-  return (
+ return (
     <div>
-      
-      <Banner/>
-      <SecondSection/>
-      <Countup/>
-      <ServicesSection/>
-      <ContactSection/>
-      <Testimonial/>
-      <TeamMember/>
+      <Banner />
+      <SecondSection />
+      <Countup />
+      <ServicesSection />
+      <ContactSection />
+      <Testimonial />
+      <TeamMember />
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
