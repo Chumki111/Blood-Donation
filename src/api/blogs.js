900c2a3f3992ab0d2a58f07dcb1cac490c65bdda
Chumki khatun <chumkikhatun370@gmail.com>
2024-07-11 13:55:29 +0600

    fetch single blog

--- src/api/blogs.js
+++ src/api/blogs.js
@@ -5,3 +5,8 @@ export const getAllBlogs=async()=>{
     const {data} = await axiosSecure('/blogs');
     return data;
 }
+// fetch single blog
+export const getSingleBlog=async(id)=>{
+    const {data} = await axiosSecure(`/blog/${id}`);
+    return data;
+}
